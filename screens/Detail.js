ead95c548009ede1c6248f20ec4c8705f737d94e
vaibhavworldwide <vaibhavsingh1008@gmail.com>
2021-12-27 10:42:23 +0530

    first commit

---
+++ screens/Detail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, ScrollView} from 'react-native';
+const Detail = () => {
+  return (
+    <React.Fragment>
+      <ScrollView style={{backgroundColor: '#000'}}>
+        <Text>{'Movie Detail'}</Text>
+      </ScrollView>
+    </React.Fragment>
+  );
+};
+
+export default Detail;
